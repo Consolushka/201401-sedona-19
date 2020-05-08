eb98a0dceb7f735be225334e0829002baee21e9e
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2020-05-09 00:57:54 +0300

    Модал + критерии v.2

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,8 +2,12 @@ var btnOpen = document.querySelector(".header__btn--menu");
 var btnClose = document.querySelector(".menu-list__item-btn--close");
 var menu = document.querySelector(".header__navigation");
 btnOpen.addEventListener("click", function () {
+  console.log("open");
   menu.classList.remove("modal-disnone");
+  btnOpen.classList.add("modal-disnone");
 });
 btnClose.addEventListener("click", function () {
+  console.log(btnOpen);
+  btnOpen.classList.remove("modal-disnone");
   menu.classList.add("modal-disnone");
 });
--- source/js/modal.js
+++ source/js/modal.js
@@ -1,9 +1,10 @@
-var btnOpen = document.querySelector(".review__btn");
+var btnOpenModal = document.querySelector(".review__btn");
 var modalAll = document.querySelectorAll(".booking");
 var btnCloseAll = document.querySelectorAll(".booking__btn");
-//var rand = Math.random();
-//rand = (rand*10).toFixed(0);
-btnOpen.addEventListener("click", function (e) {
+var form = document.querySelector(".review__form");
+var required = form.querySelectorAll(".form__required");
+
+btnOpenModal.addEventListener("click", function (e) {
   modalAll.forEach(element => {
     element.classList.remove("modal-disnone");
   });
@@ -18,4 +19,8 @@ btnCloseAll.forEach(button => {
       document.querySelector(".booking--failure").classList.add("modal-disnone");
     }
   })
+});
+
+form.addEventListener("submit", function(e){
+
 });
