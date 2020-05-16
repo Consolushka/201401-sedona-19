cc344804e29883ff6e5aaed93d6b8675267248f8
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2020-05-16 20:56:06 +0300

    Final v.1 BlessRNG

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,12 +3,11 @@ console.log(btnOpen);
 btnOpen.classList.remove("no-js");
 var menu = document.querySelector(".header__navigation");
 btnOpen.addEventListener("click", function () {
-  if(btnOpen.classList.contains("header__btn--close")){
+  if (btnOpen.classList.contains("header__btn--close")) {
     console.log("click");
     btnOpen.classList.remove("header__btn--close");
     menu.classList.add("modal-disnone");
-  }
-  else{
+  } else {
     btnOpen.classList.add("header__btn--close");
     menu.classList.remove("modal-disnone");
   }
